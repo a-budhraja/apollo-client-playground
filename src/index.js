884bee116d455c4e7754c23a3a7e2c9e37f8d503
Anubhav <anubhav.budhraja@gmail.com>
2021-02-05 11:58:29 +0100

    Initial setup with React and Apollo client

--- src/index.js
+++ src/index.js
@@ -4,6 +4,52 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { gql } from '@apollo/client';
+
+// const httpLink = createHttpLink({
+//   uri: 'https://34.70.203.107:8000/graphql/',
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     }
+//   }
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache()
+// });
+
+const client = new ApolloClient({
+  uri: 'https://34.70.203.107:8000/graphql/',
+  cache: new InMemoryCache()
+});
+
+const VERIFY_TOKEN = gql`
+mutation VerifyToken($token: String) {
+  verifyToken (token: $token){
+    payload
+  }
+}
+`;
+
+client
+  .mutate({
+    mutation: VERIFY_TOKEN,
+    variables: {token: localStorage.getItem("token")}
+  })
+  .then(result => console.log(result));
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
